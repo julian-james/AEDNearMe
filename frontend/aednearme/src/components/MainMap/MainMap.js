e6443ece59d307ea080fd149c1a1b0ee79e93dcd
unknown <b_lee12@hotmail.co.uk>
2022-02-25 14:43:12 +0000

    maps

--- frontend/aednearme/src/components/MainMap/MainMap.js
+++ frontend/aednearme/src/components/MainMap/MainMap.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { GoogleMap, useLoadScript, Marker, InfoWindow, LoadScript } from "@react-google-maps/api";
 import mapStyles from './mapStyles';
+import AddIcon from '@mui/icons-material/Add';
 
 const MainMap = () => {
 
@@ -18,20 +19,19 @@ const MainMap = () => {
     zoomControl: true
   }
 
-//   const Marker = {
-//       postion: { lat: 51.496576, lng: -0.134824},
-//       title: "AED here"
-//   }
+  const position = { lat: 51.496576, lng: -0.134824}
+     
+  
  
 
 const aedInfo = () => {
-    <InfoWindow>
-        <div>AED here</div>
+    <InfoWindow position={position}>
+        <div><h2>AED here</h2></div>
     </InfoWindow>
 }
 
 
-// AED Marker is hard coded atm
+// AED Marker is hard coded atm, change to environment variables
   return (
     <LoadScript googleMapsApiKey="AIzaSyATeYFTD2ha1aawscSrtZxJfJ3m89DB_JU">
         <GoogleMap 
@@ -39,7 +39,11 @@ const aedInfo = () => {
             zoom={17} 
             center={center} 
             options={options}>
-        <Marker position={{lat: 51.496576, lng: -0.134824}} onClick={aedInfo}/>
+        <Marker 
+            position={{lat: 51.496576, lng: -0.134824}}
+            onClick={aedInfo}>   
+        </Marker>
+        
         </GoogleMap>
 
         
