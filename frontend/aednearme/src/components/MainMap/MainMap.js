3a105adc73e06b0ce82b743ad83f5d1c320ab289
unknown <b_lee12@hotmail.co.uk>
2022-02-25 12:31:47 +0000

    still doing marker

--- frontend/aednearme/src/components/MainMap/MainMap.js
+++ frontend/aednearme/src/components/MainMap/MainMap.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoogleMap, useLoadScript, Marker, InfoWindow, LoadScript } from "@react-google-maps/api";
 import mapStyles from './mapStyles';
 
@@ -8,21 +8,41 @@ const MainMap = () => {
       width: '60vw',
       height: '60vh',
   };
+  // Hard coded center at Futureproof
   const center = {
       lat: 51.4967001,
       lng: -0.1356203
   }
   const options = {
-    styles: mapStyles
+    styles: mapStyles,
+    zoomControl: true
   }
 
-  
-//   if (loadError) return "Error loading maps"
-//   if(!isLoaded) return "Loading Maps"; 
+//   const Marker = {
+//       postion: { lat: 51.496576, lng: -0.134824},
+//       title: "AED here"
+//   }
+ 
 
+const aedInfo = () => {
+    <InfoWindow>
+        <div>AED here</div>
+    </InfoWindow>
+}
+
+
+// AED Marker is hard coded atm
   return (
     <LoadScript googleMapsApiKey="AIzaSyATeYFTD2ha1aawscSrtZxJfJ3m89DB_JU">
-        <GoogleMap mapContainerStyle={containerStyle} zoom={17} center={center} options={options}></GoogleMap>
+        <GoogleMap 
+            mapContainerStyle={containerStyle} 
+            zoom={17} 
+            center={center} 
+            options={options}>
+        <Marker position={{lat: 51.496576, lng: -0.134824}} onClick={aedInfo}/>
+        </GoogleMap>
+
+        
     </LoadScript>
   )
 }
