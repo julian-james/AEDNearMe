b28e06c599e84c56ff0052d39a5fcbd4c51956d8
unknown <b_lee12@hotmail.co.uk>
2022-02-25 14:49:25 +0000

    more routes

--- frontend/aednearme/src/App.js
+++ frontend/aednearme/src/App.js
@@ -4,6 +4,9 @@ import Home from "./pages/Home/Home";
 import Quiz from "./pages/Quiz/Quiz";
 import CPR from "./pages/CPR/CPR";
 import Choking from "./pages/Choking/Choking";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Upload from "./pages/Upload/Upload";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Footer } from "./components/Footer/Footer.js";
 
@@ -17,6 +20,9 @@ function App() {
           <Route path='/quiz' element={<Quiz />} />
           <Route path='/cpr' element={<CPR />} />
           <Route path='/choking' element={<Choking />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/upload' element={<Upload />} />
           <Route path='*' element={<p>nothing to see here, move on mate.</p>} />
       </Routes>
 
