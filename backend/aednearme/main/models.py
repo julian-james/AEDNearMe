1404fcca0080b90e5faf523fc12be60322f402c2
Julian James <julian@Julians-MacBook-Pro.local>
2022-02-28 15:25:34 +0000

    base64 image upload

--- backend/aednearme/main/models.py
+++ backend/aednearme/main/models.py
@@ -13,7 +13,7 @@ class Defib(models.Model):
     long = models.CharField(max_length=50)
     lat = models.CharField(max_length=50)
     what3words_link = models.URLField(null=True)
-    photo_url = models.URLField(null=True, blank=True)
+    photo_url = models.TextField(null=True, blank=True)
     time_taken = models.DateTimeField(auto_now_add=True)
     access = models.CharField(max_length=50, choices=ACCESS_CHOICES, default='unknown')
     approved = models.BooleanField(default=False)
--- frontend/aednearme/src/pages/Upload/Upload.js
+++ frontend/aednearme/src/pages/Upload/Upload.js
@@ -1,4 +1,5 @@
 import { Button, FormControl, Input, InputLabel, MenuItem, Paper, Select, TextField } from '@mui/material'
+import axios from 'axios';
 import React, { useState } from 'react'
 import UploadMap from '../../components/UploadMap/UploadMap'
 
@@ -7,15 +8,49 @@ const Upload = () => {
   const [ latitude, setLatitude ] = useState("");  
   const [ longitude, setLongitude ] = useState("");
   const [ access, setAccess ] = useState("");  
+  const [ uploadImage64, setUploadImage64] = useState("");
+  const [ comments, setComments ] = useState("");
 
   const handleSubmit = () => {
-      // submits upload info to database for admin to approve
-      // email confirmation
+      const data = {
+          "lat": latitude,
+          "long": longitude,
+          "access": access,
+          
+      }
+      axios.post('https://localhost:8000/aed/upload/', data, options)
   }  
 
   const handleLat = (e) => setLatitude(e.target.value);
   const handleLng = (e) => setLongitude(e.target.value);
   const handleAccess = (e) => setAccess(e.target.value);
+  const handleComments = (e) => setComments(e.target.value);
+
+const encodeImageFileAsURL = async (e) => {
+      const file = e.target.files[0]
+      const base64 = await convertBase64(file)
+      console.log(base64)
+      setUploadImage64(base64)
+    // var file = e.target[0];
+    // var reader = new FileReader();
+    // reader.onloadend = function() {
+    //   console.log('RESULT', reader.result)
+    // }
+    // reader.readAsDataURL(file);
+  }
+
+  const convertBase64 = (file) => {
+      return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(file);
+          fileReader.onload = () => {
+              resolve(fileReader.result)
+          }
+          fileReader.onerror = (error) => {
+              reject(error)
+          }
+      })
+  }
 
   return (
     <div>
@@ -24,7 +59,7 @@ const Upload = () => {
         <UploadMap />
         <FormControl color="secondary">
             <FormControl margin="normal">
-               <Input type="file" />
+               <Input onChange={(e) => {encodeImageFileAsURL(e)}} type="file" />
                <Button variant="raised">Upload img</Button> 
             
             </FormControl>
@@ -57,6 +92,14 @@ const Upload = () => {
                     <MenuItem value="Public Access">Public Access</MenuItem>
                     <MenuItem value="Limited Access">Limited Access</MenuItem>
                 </Select>
+            </FormControl>
+            <FormControl>
+                <TextField 
+                    label="Comments..."
+                    id="Comments"
+                    type="text"
+                    onChange={handleComments}
+                />
             </FormControl>
             <FormControl margin="normal">
             <Button variant="contained" onClick={handleSubmit}>Submit</Button>
