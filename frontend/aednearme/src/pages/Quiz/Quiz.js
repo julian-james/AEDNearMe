6e0b5e7d7be25b035eb74a25440e12378afa3133
unknown <b_lee12@hotmail.co.uk>
2022-03-01 09:44:13 +0000

    quiz category states

--- frontend/aednearme/src/pages/Quiz/Quiz.js
+++ frontend/aednearme/src/pages/Quiz/Quiz.js
@@ -1,23 +1,36 @@
 import { Button, FormControl, InputLabel, MenuItem, Paper, Select } from '@material-ui/core';
-import React from 'react'
+import React, { useState } from 'react'
 import './Quiz.css';
 
 
 const Quiz = () => {
+
+  const [ category, setCategory ] = useState("")
+
+  const handleCategory = (e) => setCategory(e.target.value)
+  const handleStart = () => {
+    
+  }
+
   return (
     <div style={{    padding: "40px"   }}>
     <Paper>
     <FormControl>
       <FormControl>
-        <InputLabel>Category</InputLabel>
+        <InputLabel id="category-label">Category</InputLabel>
         <Select
-          label="Category">
-          <MenuItem>CPR</MenuItem>
-          <MenuItem>Choking</MenuItem>
+          id="category"
+          label="Category"
+          value={category}
+          onChange={handleCategory}>
+          <MenuItem value="cpr">CPR</MenuItem>
+          <MenuItem value="choking">Choking</MenuItem>
         </Select>
       </FormControl>
       <FormControl>
-        <Button variant="contained">Start Quiz</Button>
+        <Button 
+          variant="contained"
+          onClick={handleStart}>Start Quiz</Button>
       </FormControl>
     </FormControl>
     </Paper>
