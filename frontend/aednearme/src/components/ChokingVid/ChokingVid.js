f4c73117dd5cb10c7b7f7246007e07d8526fdb76
Marina Ivanova <90510429+wotaque@users.noreply.github.com>
2022-03-02 10:35:37 +0000

    choking

--- frontend/aednearme/src/components/ChokingVid/ChokingVid.js
+++ frontend/aednearme/src/components/ChokingVid/ChokingVid.js
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player'
 const ChokingVid = () => {
   return (
     <div>
-        <h2>Abdominal Thrusts Tutorial</h2>
+        {/* <h2>Abdominal Thrusts Tutorial</h2> */}
         <ReactPlayer width="75%" url="https://www.youtube.com/watch?v=PA9hpOnvtCk" />
     </div>
   )
